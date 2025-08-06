2efd639c74653f8ceaefe58b7b2d5f97e08e20b3
Amine KHEDDAOUI <61601575+AmineKheddaoui@users.noreply.github.com>
2025-08-06 11:12:47 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    // Base de données OVH
+    // Remplacez ces valeurs par vos vrais paramètres OVH
+    'db_host' => 'accessb992.mysql.db',  // Serveur MySQL OVH
+    'db_name' => 'accessb992',           // Nom de votre base OVH
+    'db_user' => 'accessb992',           // Utilisateur OVH
+    'db_pass' => 'Kheddaoui1',     // Mot de passe BDD
+
+    // Email
+    'admin_email' => 'contact@accessi-web.com',
+    'from_email'  => 'contact@accessi-web.com',
+    
+    'smtp_host' => 'ssl0.ovh.net',
+    'smtp_port' => 465,
+    'smtp_user' => 'contact@accessi-web.com',
+    'smtp_pass' => 'contact_accessi-web',
+];
+?>
